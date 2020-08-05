f8d41cdb17dd298271460e865ad08048d846ca4c
Artur Dick <artur.dick@mailbox.org>
2020-08-05 15:36:34 +0200

    Modify the openzeppelin import

--- backend/contracts/TokenRepository.sol
+++ backend/contracts/TokenRepository.sol
@@ -2,8 +2,7 @@
 // @author Artur Dick
 pragma solidity >=0.5.0 <0.7.0;
 
-//import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
-import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TokenRepository is ERC721 {
     address _owner;
